25ab7a4a9f671d3098b8229a79e65ea7397c0832
LelamTech <ryanmalel87@gmail.com>
2025-10-17 17:10:11 +0300

    Task 0: Implemented Book class with magic methods

--- oop/task0/book_class.py
+++ oop/task0/book_class.py
@@ -1,25 +1,14 @@
-class Bank:
-    def __init__(self, name, balance):
-        self.name = name
-        self.balance = balance
-
-    def deposit(self, amount):
-        self.balance += amount
-
-    def withdraw(self, amount):
-        if amount <= self.balance:
-            self.balance -= amount
-        else:
-            print("Insufficient funds")
-
-    def __str__(self):
-        return f"Bank Account({self.name}, Balance: {self.balance})"
-
-
 class Book:
-    def __init__(self, title, author):
+    def __init__(self, title, author, year):
         self.title = title
         self.author = author
+        self.year = year
 
     def __str__(self):
-        return f"Book: {self.title} by {self.author}"
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+
+    def __del__(self):
+        print(f"Deleting {self.title}")
--- oop/task0/main.py
+++ oop/task0/main.py
@@ -1,13 +1,10 @@
-from book_class import Bank, Book
+from book_class import Book
 
 def main():
-    acc = Bank("Ryan", 1000)
-    acc.deposit(500)
-    acc.withdraw(300)
-    print(acc)
-
-    my_book = Book("Python 101", "Guido van Rossum")
+    my_book = Book("1984", "George Orwell", 1949)
     print(my_book)
+    print(repr(my_book))
+    del my_book
 
 if __name__ == "__main__":
     main()
